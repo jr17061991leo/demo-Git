02316c8357a084fe57f51d3f437a73850984809c
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 16:54:53 -0300

    acrescentada mensagem de boa noite

--- src/demo.java
+++ src/demo.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class demo {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.println("bom dia");
-System.out.println("boa tarde");		
-		
+		System.out.println("boa tarde");
+		System.out.println("boa noite");
 
 	}
 
