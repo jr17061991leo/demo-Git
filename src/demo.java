3a1fd515db8924efd3ff3cac3cde6359a3b13677
jr17061991leo <jr17061991leo@gmail.com>
2019-09-23 16:16:53 -0300

    projeto criado

---
+++ src/demo.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("somente para iniciar git hub");
+		
+
+	}
+
+}
